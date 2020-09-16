73046fea329c76b985bb7af6860b530ae62e01ca
dasteixeira <dasteixeira@github.com>
2020-09-16 18:45:25 -0300

    Alterando o código para ser mais legível

--- js/timeNumeros.js
+++ js/timeNumeros.js
@@ -1,36 +1,45 @@
-var botao = document.querySelector('#botaoSorteio');
+var botaoSorteio = document.querySelector('#botaoSorteio');
+var botaoRemover = document.querySelector('#botaoRemover');
 var ativo= false;
 
-function resultadoNumeros(){ // adicona o resultado no html
-    var localTimeA = document.querySelector('.tituloA')
-    var tagP = document.createElement('p');
-    tagP.classList.add('listaA');
-    localTimeA.appendChild(tagP);
-    tagP.textContent = timeA;
+// adicona o resultado no html
+function resultadoNumeros(){
 
-    var localTimeB = document.querySelector('.tituloB')
-    var tagP = document.createElement('p');
-    tagP.classList.add('listaB');
-    localTimeB.appendChild(tagP);
-    tagP.textContent = timeB;
+    let localTimeA = document.querySelector('#caixa__time-a')
+    let timeAtagP = document.createElement('p');
+    timeAtagP.classList.add('listaA');
+    localTimeA.appendChild(timeAtagP);
+    timeAtagP.textContent = timeA;
+
+    let localTimeB = document.querySelector('#caixa__time-b')
+    let timeBtagP = document.createElement('p');
+    timeBtagP.classList.add('listaB');
+    localTimeB.appendChild(timeBtagP);
+    timeBtagP.textContent = timeB;
+
+    console.log('Time A:',timeA);
+    console.log('Time B:',timeB);
 
     ativo=true;
 }
 
-document.querySelector('#botaoRemover').onclick = function(){ // remove a tag com os times numericos dos times;
-    limparDados();
-    let listaA = document.querySelector('.listaA')
-    let listaB = document.querySelector('.listaB')
-
-    listaA.remove();    
-    listaB.remove();
-    window.location.reload(); 
-}
-
-botao.addEventListener('click', function () {// adicona e alerta que sorteio foi feito 
+// adicona e alerta que sorteio foi feito
+botaoSorteio.addEventListener('click', () => { 
     if(!ativo){
         resultadoNumeros();
     }else if(ativo){
         alert('Sorteio já realizado!');
     }
 });
+
+// remove os times 
+botaoRemover.addEventListener('click', () => { 
+        limparDados();
+        let listaA = document.querySelector('.listaA')
+        let listaB = document.querySelector('.listaB')
+
+        listaA.remove();    
+        listaB.remove();
+        window.location.reload(); 
+    }
+);
